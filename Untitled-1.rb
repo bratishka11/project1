9f4b80f13a67c97c9f6f296fecb3cf2a6f898b34
Bruce Wayne <stan@emerhelp.ru>
2017-02-07 17:24:05 +0400

    second commit

--- Untitled-1.rb
+++ Untitled-1.rb
@@ -1,3 +1,15 @@
-5.times do
-  puts "Hello, World!"
-end
+puts "Как тебя зовут?"
+name = gets
+
+puts "Привет, #{name}"
+puts "Какой у тебя рост?"
+height = gets
+
+weight = height.to_i - 110
+
+if weight < 0
+    puts "Твой вес уже идеален"
+    else
+    puts "Твой вес равен " + " кг" 
+    end
+
